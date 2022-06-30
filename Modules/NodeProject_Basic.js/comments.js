3325d64a824c598de8fd8872db4c2d1d48aeb6c0
RAHAT <RAHAT@gmail.com>
2022-06-29 22:01:20 -0400

    to push first files to github

---
+++ Modules/NodeProject_Basic.js/comments.js
@@ -0,0 +1,8 @@
+// print age like My Name is Rahat Rahman and I am 29 years old.
+let userName = `Rahat Rahman`
+let userAge = 29 
+console.log(`My Name is ${userName} and I am ${userAge} years old`)
+
+let userName1 = `Rahat Rahman`
+let userAge1 = 29 
+console.log(`My Name is ${userName1} and I am ${userAge1} years old`)
