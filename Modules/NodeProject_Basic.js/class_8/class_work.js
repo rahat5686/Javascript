98e8964abbacab3c6363a32700a0736593be8b5b
RAHAT <RAHAT@gmail.com>
2022-07-16 09:02:27 -0400

    home_work_4.js

---
+++ Modules/NodeProject_Basic.js/class_8/class_work.js
@@ -0,0 +1,73 @@
+// const sports = ['Football', 'Soccer', 'BASKETBALL', 'Baseball', 'Rugby'];
+
+/**
+ * Print values present on even-index
+ * 
+ * eg: 
+ * Football
+ * BASKETBALL
+ * Rugby
+ */
+ console.log(sports[0]);
+ console.log(sports[2]);
+ console.log(sports[4]);
+
+ const sports = ['Football', 'Soccer', 'BASKETBALL', 'Baseball', 'Rugby'];
+ for (i = 1 ; sports.length -1; i++ )
+    if (sports % 1  ){
+        console.log(sport)
+    }
+
+
+    /**
+ * Create abbreviation for any sentence
+ * 
+ * 'have a great day'   ->  'HAGD'
+ * 'YOu lIVe ONlY' ->  'YLO'
+ * 'yOu neVER WaLK alOne in liFe'   ->  'YNWAIL'
+ * 'good Morning'   -> 'GM'
+ * 
+ */
+
+     let word = 'have a great day'
+
+     const word_split_space = word.split(' ');
+     console.log(`\nsplit by " " -> ${word_split_space}`);
+     let firstLetterUppercase5 = word.substring(0, 1).toUpperCase();
+     let firstLetterUppercase6 = word.substring(5, 6).toUpperCase();
+     let firstLetterUppercase7 = stmt1.substring(7, 8).toUpperCase()
+     let firstLetterUppercase8 = stmt1.substring(13, 14).toUpperCase()
+   
+     console.log(`${firstLetterUppercase5}${firstLetterUppercase6}${firstLetterUppercase7}${firstLetterUppercase8}`);
+
+
+
+
+
+
+
+// Sentence
+const originalSentence2 = 'have a great day';
+//covert the sentence to lowercase
+let lowercaseSentence2 = originalSentence2.toLowerCase();
+//Split the sentence
+let mySplitSentence2 = lowercaseSentence.split(' ');
+let titleCaseSentence2 = myHUpperCase + myAupperCase + myGLetterUpperCase + myDcapitolized;
+console.log(`The title case word is: ${titleCaseSentence2}`);
+    console.log(mySplitSentence[1].charAt(i));
+
+
+    /**
+ * print the array-values in reverse order
+ * 
+ * eg:
+ * const sports = ['Football', 'Soccer', 'BASKETBALL', 'Baseball', 'Rugby'];
+ * 
+ * Rugby
+ * Baseball
+ * BASKETBALL
+ * Soccer
+ * Football
+ * 
+ */
+     const  spo = ['Football', 'Soccer', 'BASKETBALL', 'Baseball', 'Rugby'];
